241efeff1db8f4c9c19a584640b1d70862dbf964
Olawoyin Praise <109415081+Temiyoluwa@users.noreply.github.com>
2024-05-03 01:41:16 +0100

    Update 5.Data analysis.sql

--- credit_card_fraud_analysis/5.Data analysis.sql
+++ credit_card_fraud_analysis/5.Data analysis.sql
@@ -43,51 +43,61 @@ FROM(
 WHERE Duplicate_Row > 1
 --No duplicates were found, else I would have updated code to remove duplicates
 
---Displaying records in the Tables
-SELECT * FROM TRANSACTIONS
-SELECT * FROM Credit_Card
-SELECT * FROM Card_Holder
-SELECT * FROM Merchant_Category
-SELECT * FROM Merchant_Names
-
 --DATA ANALYSIS
 
 --WHAT MERCHANT  HAD THE MOST TRANSACTIONS AND WHAT WAS THE TOTAL TRANSACTION AMOUNt
 SELECT 
-	MN.Merchant_NAME, COUNT (TR.TRANSACTION_ID) as [Total Number of Transaction by Merchant], SUM(TR.Transaction_Amount) as [Total Transaction Amount]
-FROM
-	TRANSACTIONS TR
-JOIN 
-	Merchant_Names MN
-ON 
-	TR.Merchant_Name_ID = MN.Merchant_Name_ID
-GROUP BY
-	Merchant_NAME
-ORDER BY
-	[Total Number of Transaction by Merchant], [Total Transaction Amount] DESC
-GO
+MN.Merchant_NAME, COUNT (TR.TRANSACTION_ID) as [Total Number of Transaction by Merchant], SUM(TR.Transaction_Amount) as [Total Transaction Amount]
+FROM TRANSACTIONS TR
+JOIN Merchant_Names MN
+ON TR.Merchant_Name_ID = MN.Merchant_Name_ID
+GROUP BY Merchant_NAME
+ORDER BY [Total Number of Transaction by Merchant] DESC, [Total Transaction Amount] DESC
+
 
 --What is the average transaction amount
 SELECT AVG(Transaction_Amount) 
 FROM TRANSACTIONS
 
---who are the top 5 card holder with the most transactions
-SELECT 
-	TOP 5 Card_Holder_NAME,
-	COUNT(Transaction_Id) AS total_transcations
-FROM
-	Card_Holder AS CH
-JOIN
-	Credit_Card AS CR
-ON CH.Card_Holder_ID = CR.Card_Holder_ID
-JOIN
-	TRANSACTIONS AS TR
-ON TR.Credit_Card_Number = CR.Credit_Card_Number
+--What are the Top 5 highest transactions carried out ?
+select TOP 5
+	CH.Card_Holder_ID, MAX(Transaction_Amount)[Highest Transaction Amount], Merchant_Category_Name, Merchant_NAME
+FROM TRANSACTIONS TR
+JOIN Merchant_Names MN ON MN.Merchant_Name_ID = TR.Merchant_Name_ID
+JOIN Merchant_Category MC ON MC.Merchant_Category_ID = MN.Merchant_Category_ID
+JOIN Credit_Card CC ON CC.Credit_Card_Number = TR.Credit_Card_Number
+JOIN Card_Holder CH ON CH.Card_Holder_ID = CC.Card_Holder_ID
+GROUP BY CH.Card_Holder_ID, Transaction_Amount, Merchant_Category_Name, Merchant_NAME
+ORDER BY [Highest Transaction Amount] DESC
+GO
+
+--Most credit card fraud are carried out in an unsuspecting manner. such transactions can be less than or equal to $2.00, hence reason for this analysis 
+----what month has the highest fraud transaction?
+SELECT [Transaction Month], SUM([countS of transactions]) AS Fraud_Transactions
+FROM(
+	SELECT FORMAT(TransactionDate,'MMM') AS Transaction_Month , COUNT(Transaction_Id) AS total_Transactions
+	FROM TRANSACTIONS TR
+		JOIN Merchant_Names MN ON MN.Merchant_Name_ID = TR.Merchant_Name_ID
+		JOIN Merchant_Category MC ON MC.Merchant_Category_ID = MN.Merchant_Category_ID
+		JOIN Credit_Card CC ON CC.Credit_Card_Number = TR.Credit_Card_Number
+		JOIN Card_Holder CH ON CH.Card_Holder_ID = CC.Card_Holder_ID
+	WHERE Transaction_Amount <= 2.00 --AND DATEPART(HOUR, TransactioN_Time)  NOT IN (7,9)
 GROUP BY
-	CH.Card_Holder_NAME
-ORDER BY total_transcations DESC
+	TransactionDate) AS [Fraud Transaction Month]
+GROUP BY
+	[Transaction Month]
+ORDER BY
+	[Total Fraud Transactions] DESC
 
-
+--What are the Top Five merchants prone to being hacked using small Transactions in early morning of the day
+SELECT TOP(5) Merchant_NAME, COUNT(Transaction_Id) AS small_transaction_count
+FROM TRANSACTIONS TR
+JOIN Merchant_Names MN ON MN.Merchant_Name_ID = TR.Merchant_Name_ID
+JOIN Merchant_Category MC ON MC.Merchant_Category_ID = MN.Merchant_Category_ID
+WHERE Transaction_amount <= 2.00  
+AND DATEPART(HOUR, Transaction_Time) BETWEEN 7 AND 9 
+GROUP BY Merchant_Name
+ORDER BY [Nuber of Transactions] DESC
 
 
 
